da0dd319811842496da0856c7b409ec56031fd8b
maominzhe <maominzhe93@gmail.com>
2023-07-30 14:44:46 +0200

    first commit

---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,17 @@
+package com.mashibing.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: Minzhe Mao
+ * @Date: 30.07.23 -07 - 30
+ * @Description: com.mashibing.apipassenger
+ * @Version: 1.0
+ **/
+@SpringBootApplication
+public class ApiPassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
